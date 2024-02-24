e1d98dc3ac89e3b562252725eb60dd140c9411f5
Malek Odibat <132284869+Malek1Alk7alil2@users.noreply.github.com>
2024-02-24 19:41:07 +0300

    Add files via upload

---
+++ Quiz1.js
@@ -0,0 +1,17 @@
+let numbers  = [1,2,3,4,5]
+let multiplyArr = numbers.map((numbers) => {
+   return numbers * 2
+})
+
+let indexes = []
+for(let i =0 ; i<numbers.length ; i++){
+   indexes.push(i)
+}
+console.log(numbers)
+console.log(multiplyArr)
+console.log(indexes)
+
+
+
+
+
